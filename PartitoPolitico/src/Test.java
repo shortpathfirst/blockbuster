aee937fcc84aa475f1330b40fc4768f10796e9ed
Andrea <Andrea@192.168.1.101>
2019-02-06 21:24:48 +0100

    Partito

--- PartitoPolitico/src/Test.java
+++ PartitoPolitico/src/Test.java
@@ -1,14 +1,22 @@
 import model.*;
 import controller.*;
+import view.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		GestorePartito gestore = new GestorePartito("Partito politico");
 		
 		Area radice = new AreaRadice("Radice","Lazio","Roma","municipio1","quartiere1","cap1");
+		Area areaInterna1 = new AreaInterna("area1","Lazio","Roma","municipio1","quartiere1","cap1",radice);
+		Area areaInterna2 = new AreaInterna("area2","Lazio","Roma","municipio1","quartiere1","cap1",radice);
+		Area areaFoglia = new AreaFoglia("foglia","Lazio","Roma","municipio1","quartiere1","cap1",areaInterna2);
 		
+		areaFoglia.aggiungiVotante(new CartaIscritto(1,new Iscritto("tizio","qua","1234","@123")));
+		areaFoglia.aggiungiVotante(new CartaIscritto(2,new Iscritto("caio","qua","1234","@123")));
 		
-		gestore.calcoloNumeroVotzioniSuArea(radice);
+		int num = gestore.calcoloNumeroVotzioniSuArea(radice);
+		new Stampa().stampa(""+num);
 
 	}
 
